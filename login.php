3d8f32a873b0bdf34798b252e702df7da6986c12
LoganTra <150308919+LoganTra@users.noreply.github.com>
2023-11-13 16:43:18 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form method="post" action="login.php">
+        <h1>Login</h1>
+        <div class="textbox">
+            <input type="text" placeholder="username" name="username">
+        </div>
+        <div class="textbox">
+            <input type="password" placeholder="password" name="password">
+        </div>
+        <input type="submit" value="Login" class="loginbtn" name="login_btn">
+        <div class="signup">
+            Don't have an account?
+            </br>
+            <a herf="#">Sign up</a>
+        </div>
+    </form>
+
+</body>
+
+</html>
+
+<?php
+$conn = mysqli_connect("localhost", "root","");
+if(isset($_POST['login_btn'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sql= "SELECT * FROM websitelogin.logindetails WHERE username = '$username'";
+    $result = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $resultPassword = $row['password'];
+        if($password == $resultPassword){
+            header('Location:index.html');
+        }else{
+            echo "<script>
+                alert('Login unseccessful');
+            </script>";
+        }
+    }
+}
+
+
+?>
